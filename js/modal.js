42a056178a503117fb5f07ba6c57cb10e2815240
Kravchenko Maksym <maks.cross@gmail.com>
2024-10-22 17:42:37 +0300

    fix modal scroll

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,7 @@ const btnClose = document.getElementById('myModal-close-button');
 function checkEscapePress(e) {
   if (e.key === 'Escape') {
     modalOverlay.classList.remove('open');
+    document.body.style.overflowY = 'auto';
     window.removeEventListener('keydown', checkEscapePress);
   }
 }
@@ -12,11 +13,13 @@ function checkEscapePress(e) {
 function openModal() {
   modalOverlay.classList.add('open');
   window.addEventListener('keydown', checkEscapePress);
+  document.body.style.overflowY = 'hidden';
 }
 
 function closeModal(e) {
   if (e.target === modalOverlay || e.target === btnClose) {
     modalOverlay.classList.remove('open');
+    document.body.style.overflowY = 'auto';
   }
 }
 
